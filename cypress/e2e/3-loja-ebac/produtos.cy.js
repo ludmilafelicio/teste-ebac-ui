7c505c1601c02038affb31c1ebbe9ada3c96ebcb
Ludmila Felicio <ludmila_felicio@hotmail.com>
2025-04-05 19:25:53 -0300

    page object

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,21 +1,30 @@
 ///<reference types="cypress"/>
+import produtosPage from "../../support/page-objects/produtos.page";
+
 
 describe('Funcionalidade: Produtos', () => {
 
     beforeEach(() => {
-        cy.visit('produtos')
+        produtosPage.visitarUrl()
     });
     
     it('Deve selecionar um produto da lista', () => {
-        cy.get(' .product-block ')
-            //.first()
-            //.last()
-            //.eq(2)
-            .contains('Aero Daily Fitness Tee')
-            .click()
-
+        produtosPage.buscarProdutoLista('Apollo Running Short')
             cy.get('#tab-title-description > a').should('contain', 'Descrição')
             
 
+    });
+    it.only('Deve buscar um produto com sucesso', () => {
+        let produto = 'Aether Gym Pant'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain', produto)
+    });
+
+    it('Deve visitar a página do produto', () => {
+        
+    });
+
+    it('Deve adicionar produto ao carrinho', () => {
+        
     });
 });
