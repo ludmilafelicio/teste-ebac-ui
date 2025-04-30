4413fef04869f75d07b59bff65754d1ff4105c3d
Ludmila Felicio <ludmila_felicio@hotmail.com>
2025-04-30 16:55:17 -0300

    mudanca no gitignore

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: '4xg4zf',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     baseUrl: 'http://lojaebac.ebaconline.art.br/',
+    video: true,
     
   },
 });
